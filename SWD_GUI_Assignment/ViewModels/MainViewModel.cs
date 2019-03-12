14f8b102eb40f5eb1dcd34aef0a1a51254c88abf
Frank Andersen <frank.andersen@gmail.com>
2019-03-12 12:23:00 +0100

    Almost done with MainWindow

--- SWD_GUI_Assignment/ViewModels/MainViewModel.cs
+++ SWD_GUI_Assignment/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using SWD_GUI_Assignment.Models;
+﻿using System.Windows;
+using Prism.Commands;
+using SWD_GUI_Assignment.Models;
 using SWD_GUI_Assignment.Services;
 
 namespace SWD_GUI_Assignment.ViewModels
@@ -18,7 +20,35 @@ namespace SWD_GUI_Assignment.ViewModels
             _navigationService = new NavigationService();
 
             WindowTitle = "The Debt Book";
-            _debtors.Add(new Debtor());
+            var debtor = new Debtor();
+            debtor.AddTransaction(new Transaction(10));
+            _debtors.Add(debtor);
+
         }
+
+        private DelegateCommand _addDebtorCommand;
+
+        public DelegateCommand AddDebtorCommand => _addDebtorCommand ?? (_addDebtorCommand = new DelegateCommand(() =>
+        {
+            var vm = new AddDebtorViewModel(_navigationService);
+
+            if (_navigationService.Show(vm) == true)
+            {
+                //Debtors.Add(vm.Debtor);
+            }
+        }));
+
+        private DelegateCommand<Debtor> _editDebtorCommand;
+
+        public DelegateCommand<Debtor> EditDebtorCommand => _editDebtorCommand ?? (_editDebtorCommand = new DelegateCommand<Debtor>((debtor) =>
+        {
+            var vm = new EditDebtorViewModel(_navigationService);
+            MessageBox.Show("CLICKED! Balance: " + debtor.Balance.ToString());
+
+            if (_navigationService.Show(vm) == true)
+            {
+                //Debtors.Add(vm.Debtor);
+            }
+        }));
     }
 }
--- SWD_GUI_Assignment/Views/MainWindow.xaml.cs
+++ SWD_GUI_Assignment/Views/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SWD_GUI_Assignment.Models;
+using SWD_GUI_Assignment.ViewModels;
 
 namespace SWD_GUI_Assignment
 {
